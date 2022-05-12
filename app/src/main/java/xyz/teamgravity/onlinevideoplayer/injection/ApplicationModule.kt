bcc59c9495bb0bcf3ebadcb09ed26d956a3e6c41
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-12 16:26:17 +0500

    provide pexels api

---
+++ app/src/main/java/xyz/teamgravity/onlinevideoplayer/injection/ApplicationModule.kt
@@ -0,0 +1,25 @@
+package xyz.teamgravity.onlinevideoplayer.injection
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Retrofit
+import xyz.teamgravity.onlinevideoplayer.data.remote.api.PexelsApi
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun providePexelsApi(): PexelsApi = Retrofit.Builder()
+        .baseUrl(PexelsApi.BASE_URL)
+        .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+        .build()
+        .create(PexelsApi::class.java)
+}
