855ebbfea25e80db858fa77759c277fb0fd5bac8
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-13 11:58:22 +0500

    add navigation
    Took 15 seconds

--- app/src/main/java/xyz/teamgravity/onlinevideoplayer/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/onlinevideoplayer/presentation/activity/Main.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.onlinevideoplayer.presentation.navigation.Navigation
 import xyz.teamgravity.onlinevideoplayer.presentation.theme.OnlineVideoPlayerTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OnlineVideoPlayerTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Navigation()
                 }
             }
         }
