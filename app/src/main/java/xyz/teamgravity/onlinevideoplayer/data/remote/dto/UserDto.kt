8c37051fc4beadd5ebc0c79f30c74e7559d56146
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-13 14:04:43 +0500

    removed unnecessary dtos
    Took 19 seconds

--- app/src/main/java/xyz/teamgravity/onlinevideoplayer/data/remote/dto/UserDto.kt
+++
@@ -1,9 +0,0 @@
-package xyz.teamgravity.onlinevideoplayer.data.remote.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class UserDto(
-    @SerializedName("id") val id: Int,
-    @SerializedName("name") val name: String,
-    @SerializedName("url") val url: String
-)
--- app/src/main/java/xyz/teamgravity/onlinevideoplayer/data/remote/dto/VideoPictureDto.kt
+++
@@ -1,9 +0,0 @@
-package xyz.teamgravity.onlinevideoplayer.data.remote.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class VideoPictureDto(
-    @SerializedName("id") val id: Int,
-    @SerializedName("picture") val picture: String,
-    @SerializedName("nr") val nr: Int
-)
