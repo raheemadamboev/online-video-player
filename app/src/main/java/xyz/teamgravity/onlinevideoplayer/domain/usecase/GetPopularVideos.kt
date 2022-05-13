28d4ff65f976ac3006be9e015ac70534366bc17a
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-13 17:22:32 +0500

    refactoring
    Took 2 minutes

--- app/src/main/java/xyz/teamgravity/onlinevideoplayer/domain/usecase/GetPopularVideos.kt
+++ app/src/main/java/xyz/teamgravity/onlinevideoplayer/domain/usecase/GetPopularVideos.kt
@@ -2,11 +2,6 @@ package xyz.teamgravity.onlinevideoplayer.domain.usecase
 
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import xyz.teamgravity.onlinevideoplayer.R
-import xyz.teamgravity.onlinevideoplayer.core.util.Resource
-import xyz.teamgravity.onlinevideoplayer.core.util.UniversalText
 import xyz.teamgravity.onlinevideoplayer.domain.model.VideoModel
 import xyz.teamgravity.onlinevideoplayer.domain.repository.VideoRepository
 
