aee9154f110cb72eb52b903005484866c83a044b
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-13 14:42:55 +0500

    add small app bar to video list
    Took 13 minutes

--- app/src/main/java/xyz/teamgravity/onlinevideoplayer/presentation/screen/VideoListScreen.kt
+++ app/src/main/java/xyz/teamgravity/onlinevideoplayer/presentation/screen/VideoListScreen.kt
@@ -1,19 +1,16 @@
 package xyz.teamgravity.onlinevideoplayer.presentation.screen
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import xyz.teamgravity.onlinevideoplayer.R
 import xyz.teamgravity.onlinevideoplayer.presentation.component.CenteredProgressBar
 import xyz.teamgravity.onlinevideoplayer.presentation.component.VideoCard
 import xyz.teamgravity.onlinevideoplayer.presentation.viewmodel.VideoListViewModel
@@ -27,13 +24,18 @@ fun VideoListScreen(
     val videos = viewmodel.videos.collectAsLazyPagingItems()
 
     Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = { Text(text = stringResource(id = R.string.videos)) }
+            )
+        },
         snackbarHost = {
             SnackbarHost(hostState = snackbar) { data ->
                 Snackbar(snackbarData = data)
             }
         }
     ) { padding ->
-        LazyColumn(modifier = Modifier.padding(padding)) {
+        LazyColumn(contentPadding = padding) {
             if (videos.loadState.refresh == LoadState.Loading) item { CenteredProgressBar() }
 
             items(videos) { video ->
